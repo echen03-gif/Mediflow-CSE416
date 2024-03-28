f4844d41192217ad9395437fed2e2ed301bca801
Luigi Razon <luigirazon@gmail.com>
2024-03-28 03:53:47 -0400

    redundant

--- client/src/components/MediFlow.js
+++ client/src/components/MediFlow.js
@@ -6,21 +6,13 @@ import {
 } from "react-router-dom";
 import MainPage from "./MainPage";
 import LoginPage from "./LoginPage";
-import { MainPageProvider } from "./MainPageContext";
 
 export default function MediFlow() {
 	return (
         <Router>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-                <Route 
-                    path="/main/*"
-                    element={
-                        <MainPageProvider>
-                            <MainPage />
-                        </MainPageProvider>
-                    }
-                />
+                <Route path="/main/*" element={<MainPage />} />
                 <Route path="*" element={<Navigate to="/login" />} />
             </Routes>
         </Router>
