2190bf659dcbc9baaf7486e6b540306885a7ef02
brice580 <brice.joseph@stonybrook.edu>
2024-03-29 00:58:41 -0400

    testing

---
+++ cypress/e2e/App.cy.js
@@ -0,0 +1,13 @@
+import App from "../../src/App"
+import LoginPage from '../../src/components/LoginPage';
+
+describe('<App />', () => {
+    it('renders app and checks login page is present', () => {
+        
+        cy.mount(<App />);
+    
+        cy.get('h5').should('contain', 'Welcome Back, Login.');
+        cy.get('form').should('be.visible');
+      });
+
+})
