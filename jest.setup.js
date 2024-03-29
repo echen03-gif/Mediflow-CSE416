83c8dc5b8aaa50dd95df84aa261ef2a5d03d4a0c
echen03-gif <edward.chen@stonybrook.edu>
2024-03-29 06:13:50 -0400

    ENV Name Change

--- jest.setup.js
+++ jest.setup.js
@@ -3,7 +3,7 @@ const { Firestore } = require('@firebase/testing');
 
 const PROJECT_ID = 'mediflow-568ba';
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY); 
+const serviceAccount = require(process.env.FIREBASE_PRIVATE_KEY); 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   projectId: PROJECT_ID 
