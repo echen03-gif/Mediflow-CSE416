b4cb6de16117c8c640bce0a70f90bf3c19bf87e5
echen03-gif <edward.chen@stonybrook.edu>
2024-03-29 06:26:28 -0400

    GITHUB Secrets Change

--- jest.setup.js
+++ jest.setup.js
@@ -1,15 +1,14 @@
 require('text-encoding').TextDecoder;
+require('dotenv').config();
 const { Firestore } = require('@firebase/testing');
 
 const PROJECT_ID = 'mediflow-568ba';
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_PRIVATE_KEY); 
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY); 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   projectId: PROJECT_ID 
 });
 
 const db = admin.firestore();
-
-
-export { db };
