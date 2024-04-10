e64180a16655fa0814e4bf0ed9bbfe3da248b521
brice580 <brice.joseph@stonybrook.edu>
2024-04-10 19:46:09 -0400

    staff shifts

--- server/initTest.js
+++ server/initTest.js
@@ -21,6 +21,7 @@ let Processes = require('./models/processes.js');
 
 const uri = process.env.MEDIFLOWKEY;
 let mongoose = require('mongoose');
+const { schedule } = require('firebase-functions/v1/pubsub');
 
 mongoose.connect(uri);
 
@@ -28,7 +29,7 @@ let db = mongoose.connection;
 
 // Test Functions
 
-function createUser(adminBool, email, name, password, role, staffID, status){
+function createUser(adminBool, email, name, password, role, staffID, schedule){
 
     let testUser = {
         admin: adminBool,
@@ -39,7 +40,7 @@ function createUser(adminBool, email, name, password, role, staffID, status){
         processes: [],
         role: role,
         staffID: staffID,
-        status: status
+        schedule: schedule,
     }
 
     let addUser = new Users(testUser)
@@ -87,9 +88,35 @@ function createRoom(name, roomID, status, type){
 
 const populate = async () => {
     
-    let testAdmin = await createUser(true, "testadmin@gmail.com", "testAdmin", "testAdmin", "admin", 0, "NOT AVAILABLE");
-    let testDoctor = await createUser(false, "testDoctor@gmail.com", "testDoctor", "testDoctor", "doctor", 0, "ON CALL");
-    let testDoctor2 = await createUser(false, "testDoctor2@gmail.com", "testDoctor2", "testDoctor2", "doctor", 0, "ON DUTY");
+    let testAdmin = await createUser(true, "testadmin@gmail.com", "testAdmin", "testAdmin", "admin", 0,
+     {
+        Monday: [{start: "09:00", end: "17:00"}],
+        Tuesday: [{start: "09:00", end: "17:00"}],
+        Wednesday: [{start: "20:00", end: "24:00"}],
+        Thursday: [{start: "06:00", end: "19:00"}],
+        Friday: [{start: "03:00", end: "20:00"}],
+        Saturday: [{start: "15:00", end: "18:00"}],
+        Sunday: [{start: "04:00", end: "15:00"}]
+    });
+    let testDoctor = await createUser(false, "testDoctor@gmail.com", "testDoctor", "testDoctor", "doctor", 0, 
+    {
+        Monday: [{start: "05:00", end: "13:00"}],
+        Tuesday: [{start: "04:00", end: "20:00"}],
+        Wednesday: [{start: "02:00", end: "12:00"}],
+        Thursday: [{start: "10:00", end: "21:00"}],
+        Friday: [{start: "09:00", end: "13:00"}],
+        Saturday: [{start: "01:00", end: "10:00"}],
+        Sunday: [{start: "01:00", end: "10:00"}]
+    });
+    let testDoctor2 = await createUser(false, "testDoctor2@gmail.com", "testDoctor2", "testDoctor2", "doctor", 0, {
+        Monday: [{start: "10:00", end: "23:00"}],
+        Tuesday: [{start: "10:00", end: "23:00"}],
+        Wednesday: [{start: "05:00", end: "17:00"}],
+        Thursday: [{start: "01:00", end: "6:00"}],
+        Friday: [{start: "01:00", end: "10:00"}],
+        Saturday: [{start: "01:00", end: "10:00"}],
+        Sunday: [{start: "11:00", end: "15:00"}]
+    });
 
     let testRoom = await createRoom("Test Room", 0, "Open", "Radiology");
     let testEquipment = await createEquipmentHead("CT Machine", 0, "Radiology");
