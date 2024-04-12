a2831eeab6138ab41c0117588c3d908dd13754af
echen03-gif <edward.chen@stonybrook.edu>
2024-04-12 00:58:42 -0400

    Jest Test Fix Attempt

--- server/tests/Backend.test.js
+++ server/tests/Backend.test.js
@@ -1,89 +1,94 @@
 const supertest = require('supertest')
+const mongoose = require('mongoose');
 
 // import server file
-const {app, server} = require('../server.js');
+const { app, server } = require('../server.js');
 
 // Schemas
 const User = require('../models/users.js');
 
 describe('User Collection', () => {
-  
-    let testData = null;
-  
-    it('creates user', async () =>{
 
-      const testUser = {
-        name: "testUser",
-        email: "testUser@gmail.com"
-      }
+  let testData = null;
 
-      const testUserData = new User(testUser);
+  it('creates user', async () => {
 
-      testData = await testUserData.save();
+    const testUser = {
+      name: "testUser",
+      email: "testUser@gmail.com"
+    }
 
-      expect(testData.name).toBe(testUser.name);
-      expect(testData.email).toBe(testUser.email);
+    const testUserData = new User(testUser);
+
+    testData = await testUserData.save();
+
+    expect(testData.name).toBe(testUser.name);
+    expect(testData.email).toBe(testUser.email);
 
 
+  });
+
+  it('creates admin', async () => {
+
+    const testUser = {
+      admin: true,
+      name: "testAdminJest",
+      email: "testAdminJest@gmail.com"
+    }
+
+    const testUserData = new User(testUser);
+
+    testData = await testUserData.save();
+
+    expect(testData.admin).toBe(true);
+    expect(testData.name).toBe(testUser.name);
+    expect(testData.email).toBe(testUser.email);
+
+
+  });
+
+  it('creates user on Express API', async () => {
+    const testUser = {
+      name: "testUser",
+      email: "testUser@gmail.com"
+    };
+
+    testData = await supertest(app)
+      .post('/createUser')
+      .send(testUser)
+      .expect(200);
+
+
+    expect(testData.body.name).toBe(testUser.name);
+    expect(testData.body.email).toBe(testUser.email);
+
+    if (testData) {
+
+      await User.deleteOne({ _id: testData.body._id });
+
+    }
+
+
+  });
+
+
+  afterEach(async () => {
+    if (testData) {
+
+      await User.deleteOne({ _id: testData._id });
+
+    }
+
+  });
+
+  afterAll((done) => {
+
+
+    if (server) server.close(() => {
+      mongoose.disconnect().then(() => done());
     });
-
-    it('creates admin', async () =>{
-
-      const testUser = {
-        admin: true,
-        name: "testAdminJest",
-        email: "testAdminJest@gmail.com"
-      }
-
-      const testUserData = new User(testUser);
-
-      testData = await testUserData.save();
-
-      expect(testData.admin).toBe(true);
-      expect(testData.name).toBe(testUser.name);
-      expect(testData.email).toBe(testUser.email);
+    else done();
+  });
 
 
-    });
-
-    it('creates user on Express API', async () => {
-      const testUser = {
-        name: "testUser",
-        email: "testUser@gmail.com"
-      };
-  
-      testData = await supertest(app)
-        .post('/createUser') 
-        .send(testUser)
-        .expect(200);  
-      
-
-      expect(testData.body.name).toBe(testUser.name);
-      expect(testData.body.email).toBe(testUser.email);
-
-      if (testData) {
-        
-        await User.deleteOne({ _id: testData.body._id });
-        
-      }
-
-  
-    });
-
-
-    afterEach(async () => {
-      if (testData) {
-        
-        await User.deleteOne({ _id: testData._id });
-        
-      }
-
-    });
-
-    afterAll((done) => {
-      if (server) server.close(done); 
-      else done(); 
-    });
-
-  
 });
