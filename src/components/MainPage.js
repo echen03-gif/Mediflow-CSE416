3664d9e91479de56b0237704b5e981e6f6ea6267
gameboy2003 <ethan.pascual@gmail.com>
2024-04-18 22:25:48 -0400

    hopefully this works

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -38,7 +38,7 @@ const upcomingPatients = [
 
 export default function MainPage() {
   const drawerWidth = 200; 
-  const [cookies, setCookies, removeCookies] = useCookies();
+  const [cookies, setCookies, removeCookies] = useCookies(['user']);
   const navigate = useNavigate();
 
 
