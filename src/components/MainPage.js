9eea4c69d425e7fdadf1598676a9c799d8bb5f68
Tortugasaur <luigirazon@gmail.com>
2024-04-20 19:43:01 -0400

    update ui

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,11 @@
-import { React, useEffect} from "react";
-import { Routes, Route, Link, useLocation, useNavigate } from "react-router-dom";
+import { React, useEffect, useState } from "react";
+import {
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import {
   Box,
   Drawer,
@@ -11,23 +17,30 @@ import {
   Typography,
   Avatar,
   IconButton,
-  Button
+  Button,
+  AppBar,
 } from "@mui/material";
-import Schedule from "./Schedule";
-import Inventory from "./Inventory";
-import AddItem from "./AddItem";
-import Request from "./RequestAppointment";
-import Rooms from "./Rooms";
-import Staff from "./Staff";
-import Inbox from "./Inbox";
-import ChatScreen from './ChatScreen';
-import AddStaff from "./AddStaff";
-import AddInventory from "./AddInventory";
-import AddRoom from "./AddRoom";
-import CreateProcess from "./CreateProcess";
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
-
+import MenuIcon from "@mui/icons-material/Menu";
+import EventNoteIcon from "@mui/icons-material/EventNote"; // for Schedule
+import Inventory2Icon from "@mui/icons-material/Inventory2"; // for Inventory
+import PeopleIcon from "@mui/icons-material/People"; // for Staff
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom"; // for Rooms
+import MailOutlineIcon from "@mui/icons-material/MailOutline"; // for Inbox
+import LogoutIcon from '@mui/icons-material/Logout';
+import Schedule from "./mainPage/Schedule";
+import Inventory from "./mainPage/Inventory";
+import AddItem from "./mainPage/AddItem";
+import Request from "./mainPage/RequestAppointment";
+import Rooms from "./mainPage/Rooms";
+import Staff from "./mainPage/Staff";
+import Inbox from "./mainPage/Inbox";
+import ChatScreen from "./mainPage/ChatScreen";
+import AddStaff from "./mainPage/AddStaff";
+import AddInventory from "./mainPage/AddInventory";
+import AddRoom from "./mainPage/AddRoom";
+import CreateProcess from "./mainPage/CreateProcess";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 // Mock array of upcoming patients
 const upcomingPatients = [
@@ -37,33 +50,39 @@ const upcomingPatients = [
 ];
 
 export default function MainPage() {
-  const drawerWidth = 200;
-  const [cookies, , removeCookies] = useCookies(['user']);
+  const [drawerWidth, setDrawerWidth] = useState(200);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(true); // initially true if you want it open by default
+  const [cookies, , removeCookies] = useCookies(["user"]);
   const navigate = useNavigate();
   const location = useLocation();
 
+  const activeRouteStyle = {
+    backgroundColor: "#FF8C00", // Example background color for active route
+    "&:hover": {
+      backgroundColor: "#ff7900", // Darker on hover for active route
+    },
+  };
+
+
   const checkToken = () => {
-
-    axios.get("https://mediflow-cse416.onrender.com/decode", { withCredentials: true }).then(res => {
-
-      console.log(res.data);
-
-    })
-
-  }
-
+    axios
+      .get("https://mediflow-cse416.onrender.com/decode", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
 
   useEffect(() => {
     // Check if user cookie exists on component mount
     if (!cookies.user) {
       // If user cookie doesn't exist, navigate to login page
-      navigate('/login');
-    }
-    else {
-      checkToken()
+      navigate("/login");
+    } else {
+      checkToken();
     }
   }, [cookies.user, navigate]);
-
 
   const handleRefreshClick = (targetPath) => (event) => {
     console.log("hello");
@@ -71,14 +90,18 @@ export default function MainPage() {
       event.preventDefault();
       window.location.href = targetPath;
     }
-
   };
 
   const handleLogout = () => {
     // Remove user cookie
-    removeCookies('user', { path: '/' });
+    removeCookies("user", { path: "/" });
     // Navigate to login page
-    navigate('/login');
+    navigate("/login");
+  };
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+    setDrawerWidth(isDrawerOpen ? 65 : 200);
   };
 
   return (
@@ -90,60 +113,112 @@ export default function MainPage() {
         overflow: "hidden",
       }}
     >
-  
       <Drawer
         variant="permanent"
+        open={isDrawerOpen}
         sx={{
           width: drawerWidth,
-          [`& .MuiDrawer-paper`]: {
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
             width: drawerWidth,
             boxSizing: "border-box",
+            overflowX: "hidden", // Prevents horizontal scrollbar
             background: "linear-gradient(to bottom, #FFBA51, #FF4D34)",
-
           },
         }}
       >
-        <Toolbar />
         <List
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
           }}
         >
           <ListItem>
-            <Typography variant="h5" sx={{ marginBottom: 4 }}>
-              MediFlow⚕️
+            <Typography
+              variant="h5"
+              sx={{ marginBottom: 4, fontWeight: "bold" }}
+            >
+              {isDrawerOpen && "MediFlow"}🏥
             </Typography>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={Link} to="/main/schedule" onClick={handleRefreshClick("/main/schedule")}>
-              <ListItemText primary="Schedule" />
+            <ListItemButton
+              component={Link}
+              to="/main/schedule"
+              onClick={handleRefreshClick("/main/schedule")}
+              sx={
+                location.pathname === "/main/schedule" ? activeRouteStyle : {}
+              }
+            >
+              <EventNoteIcon />
+              {isDrawerOpen && (
+                <ListItemText primary=" Schedule" sx={{ fontWeight: "bold" }} />
+              )}
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={Link} to="/main/inventory" onClick={handleRefreshClick("/main/inventory")}>
-              <ListItemText primary="Inventory" />
+            <ListItemButton
+              component={Link}
+              to="/main/inventory"
+              onClick={handleRefreshClick("/main/inventory")}
+              sx={
+                location.pathname === "/main/inventory" ? activeRouteStyle : {}
+              }
+            >
+              <Inventory2Icon />
+              {isDrawerOpen && <ListItemText primary=" Inventory" />}
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={Link} to="/main/staff" onClick={handleRefreshClick("/main/staff")}>
-              <ListItemText primary="Staff" />
+            <ListItemButton
+              component={Link}
+              to="/main/staff"
+              onClick={handleRefreshClick("/main/staff")}
+              sx={location.pathname === "/main/staff" ? activeRouteStyle : {}}
+            >
+              <PeopleIcon />
+              {isDrawerOpen && <ListItemText primary=" Staff" />}
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={Link} to="/main/rooms" onClick={handleRefreshClick("/main/rooms")}>
-              <ListItemText primary="Rooms" />
+            <ListItemButton
+              component={Link}
+              to="/main/rooms"
+              onClick={handleRefreshClick("/main/rooms")}
+              sx={location.pathname === "/main/rooms" ? activeRouteStyle : {}}
+            >
+              <MeetingRoomIcon />
+              {isDrawerOpen && <ListItemText primary=" Rooms" />}
             </ListItemButton>
           </ListItem>
           <ListItem disablePadding>
-            <ListItemButton component={Link} to="/main/inbox" onClick={handleRefreshClick("/main/inbox")}>
-              <ListItemText primary="Inbox" />
+            <ListItemButton
+              component={Link}
+              to="/main/inbox"
+              onClick={handleRefreshClick("/main/inbox")}
+              sx={location.pathname === "/main/inbox" ? activeRouteStyle : {}}
+            >
+              <MailOutlineIcon />
+              {isDrawerOpen && <ListItemText primary=" Inbox" />}
             </ListItemButton>
           </ListItem>
-          <ListItem disablePadding sx={{ marginTop: 'auto', display: 'flex', justifyContent: 'center' }}>
-            <Button variant="contained" color="primary" onClick={handleLogout}>
-              Logout
+          <ListItem
+            disablePadding
+            sx={{
+              marginTop: "auto",
+              display: "flex",
+              justifyContent: "center",
+              width: isDrawerOpen ? "200px" : "50px",
+              padding: isDrawerOpen ? "16px 5px" : "0px",
+            }}
+          >
+            <Button
+              startIcon={<LogoutIcon />}
+              variant="contained"
+              color="primary"
+            >
+              {isDrawerOpen && "Logout"}
             </Button>
           </ListItem>
         </List>
@@ -155,7 +230,8 @@ export default function MainPage() {
           pl: 3,
           pr: 3,
           width: `calc(100vw - ${drawerWidth}px)`,
-          height: "calc(100vh)",
+          mt: "64px",
+          height: "calc(100vh - 64px)",
           overflow: "hidden",
           backgroundColor: "white",
         }}
@@ -173,58 +249,52 @@ export default function MainPage() {
           <Route path="chatscreen" element={<ChatScreen />} />
           <Route path="inbox" element={<Inbox />} />
           <Route path="createprocess" element={<CreateProcess />} />
-          
         </Routes>
       </Box>
 
-
-      
-      <Box
+      <AppBar
+        position="fixed"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          width: drawerWidth,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
           background: "linear-gradient(to bottom, #FFBA51, #FF4D34)",
-          p: 1,
         }}
       >
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        ></IconButton>
-        <Typography
-          variant="h4"
-          component="div"
-          sx={{ display: { xs: "none", md: "flex" } }}
-        >
-         Test User
-        </Typography>
-
-        <Avatar
-          alt="Remy Sharp"
-          src="https://mui.com/static/images/avatar/1.jpg"
-        />
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ display: { xs: "none", md: "flex" }, mb: 2 }}
-        >
-          Upcoming Patients
-        </Typography>
-        {upcomingPatients.map((patient, index) => (
-          <Box key={index} sx={{ width: "100%", textAlign: "center", mb: 1 }}>
-            <Typography variant="body1">{patient.name}</Typography>
-            <Typography variant="body2">{`Time until turn: ${patient.timeUntilTurn}`}</Typography>
-            <Typography variant="body2">{`Stage: ${patient.stage}`}</Typography>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+            sx={{ mr: 2 }}
+          />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Test User
+          </Typography>
+          <Box
+            sx={{ display: { xs: "none", md: "flex" }, alignItems: "center" }}
+          >
+            <Typography variant="body1" sx={{ marginRight: 2 }}>
+              Upcoming Patients
+            </Typography>
+            {upcomingPatients.map((patient, index) => (
+              <Box key={index} sx={{ textAlign: "center", mx: 1 }}>
+                <Typography variant="body2">{patient.name}</Typography>
+                <Typography variant="body2">{`Time until turn: ${patient.timeUntilTurn}`}</Typography>
+                <Typography variant="body2">{`Stage: ${patient.stage}`}</Typography>
+              </Box>
+            ))}
           </Box>
-        ))}
-      </Box>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 }
